54c7781733fcad79f7d1dc14923df4a3bb847928
youknowankit <thisisankityadav@gmail.com>
2024-08-02 12:04:19 +0530

    Added content to 01_variables

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,9 @@
+/*
+Today's JS is not same as old js as now it has modern features like classes, modules, arrow functions etc.
+So, JS committee didn't want that old code stops working and new code also works. So to treat 
+all code as newer version of code write "use strict"
+*/
+
 "use strict"; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs, not browser
