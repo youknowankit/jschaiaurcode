b59e36ea302a818454c1a376aaa9d6de834eac9d
youknowankit <thisisankityadav@gmail.com>
2024-08-02 11:16:58 +0530

    Added more steps

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,4 +3,9 @@ console.log("Hitesh")
 /*
 To save the changes made in VS-Code to your Git-Hub Repo follow the following steps:
 1. Go to Source Control option on side menu in VS Code
+2. The Source control tracks the changes you have made to the files
+3. To add changes click on plus icon after each change listed
+4. Type a message for the commit and click on the option commit.
+
+5. Click on triple dots and push your changes to the github repo.
 */
