cbb8aeba61a2c9ef76bcd8dc2c1543a58d24e901
youknowankit <thisisankityadav@gmail.com>
2024-08-02 11:47:02 +0530

    Added steps to use codespace provided by GitHub

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,11 +1,26 @@
 console.log("Hitesh")
 
 /*
+How to make js environment on codespace provided by github?
+1. Go to file-> View-> Command Pallete 
+2. Type Container: Add dev container config files
+3. Search Node js and click on show all definitions
+4. Click on Node.js & Javascript
+5. Select the latest version that's available
+6. Next it asks for additional tools which we don't need then select ok : It Adds Dev Container
+7. Rebuild changes
+8. Your online machine instance will be restarted 
+9. Now your file in available in 01_basics so type node 01_basics/test.js to execute it with nodejs
+10. When you close it: Go to codespaces & click on running instance and delete it to save available hours
+
+*/
+
+
+/*
 To save the changes made in VS-Code to your Git-Hub Repo follow the following steps:
 1. Go to Source Control option on side menu in VS Code
 2. The Source control tracks the changes you have made to the files
 3. To add changes click on plus icon after each change listed
 4. Type a message for the commit and click on the option commit.
-
 5. Click on triple dots and push your changes to the github repo.
 */
