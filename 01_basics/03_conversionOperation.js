91b3308b422ca16c507ff8a2cfc147eb39afd672
youknowankit <thisisankityadav@gmail.com>
2024-08-07 15:16:09 +0530

    Added explaination to 04_comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -86,26 +86,33 @@ to booleans, numbers, objects, array, etc */
 // *********************** Operations ***********************
 
 let value = 3
-let negValue = -value
-// console.log(negValue);
+let negValue = -value  //assigns negative of value to variable negValue
+console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+
+//BASIC ARITHEMATIC OPERATIONS 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
 
 let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);   
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);       //12
+console.log(1 + "2");       //12
+console.log("1" + 2 + 2);   //122
+console.log(1 + 2 + "2");   //32
+
+/*This means that if string is given first then it will be treated as a string.
+But if numbers are given first then  */
 
 // console.log( (3 + 4) * 5 % 3);
 
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,39 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+/*These basic comparison operators return a boolean value after comparison.
+i.e. True or False so these are fairly easy to understand*/
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+/*Problem arises when we compare different data types */
+console.log("2" > 1);    //true
+console.log("02" > 1);   //true
+/*It means javascript has converted string to number for comparsion. But this type of comparison doesn't
+give a predictable result. So, whenever you compare two values ensure data types must be same. */
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+  
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
+/*AVOID THESE CONVERSIONS AS THEY CREATE CONFUSIONS AND RESULTS MAY VARY. */
+console.log(null > 0);  //true
+console.log(null == 0); //true
+console.log(null >= 0); //false
+/*Why so?
+The reason is that an equality check == and comparisons <= work differently.
+Comparisons convert null to a number. treating it as 0.
+That's why 3rd statement null>=0 is true and 1st statement null>0 is false. */
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+/*It gives all values as false. */
 
-// === 
 
+
+
+/*So, learning is: Comparison and Equality check are two different things. */
+
+
+
+/*Strict Check "===" Triple Equals check values as well as data types*/
 console.log("2" === 2);
